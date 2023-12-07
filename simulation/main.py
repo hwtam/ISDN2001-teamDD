72ca5cc1d286dde51d09bc315a4d60ab71e4fdb9
tomtom <hwtamad@connect.ust.hk>
2023-12-07 14:44:21 +0800

    make fake data(

--- simulation/main.py
+++ simulation/main.py
@@ -1,12 +1,19 @@
 import minibus
 import stop
 
+### var ###
 MAX_TIME = 1000
 NUM_STOP = 7
 num_bus = 1
 
-### main ###
+##### main #####
 
-# make the bus stop
-stop.stop(0, 0.1, 0) #first stop
-stop.stop()
+### init ###
+# make the bus stop (location, P_queue, P_off)
+stop.stop(0, 5, 0) # start , most ppl get in, 0 ppl get off
+stop.stop(90, 5, .15) # 2
+stop.stop(318, 5, .35) # 3
+stop.stop(366, 5, .5) # 4
+stop.stop(404, 5, .10) # 5
+stop.stop(488, 5, .25) # 6
+stop.stop(553, 0, 1) # end , 0 ppl get in, all ppl get off
--- simulation/stop.py
+++ simulation/stop.py
@@ -3,9 +3,9 @@ class stop :
   l = [] # a static list, storing all stop
 
   def __init__(self, location = -1, P_queue = 0, P_off = 0) :
-    self.ppl = 0
-    self.time_next_arrival = -1
-    self.location = location
-    self.P_queue = P_queue
-    self.P_off = P_off
+    self.ppl = 0  # current amount of ppl
+    self.time_next_arrival = -1  # remainning time for the next arrival
+    self.location = location  # relative "location" of the stop at the route , how many sec needed to go to from the start(by real data)
+    self.P_queue = P_queue  # P(how many ppl get in the queue per time)
+    self.P_off = P_off  # P(how many ppl get off the minibus per people in bus)
     stop.l.append(self)
