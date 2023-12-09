f5b77c93f1bdca25beb969fa2674b81cd2ff4b61
tomtom <hwtamad@connect.ust.hk>
2023-12-09 22:03:14 +0800

    LINK START! (https://github.com/evnchn/linkstart.py)

--- simulation/main.py
+++ simulation/main.py
@@ -1,11 +1,45 @@
+import_string = \
+'''
 import minibus
 import stop
 import vis
 import random
 import os
 import subprocess
-import openpyxl as xl
-
+import openpyxl as xl # openpyxl
+'''
+### LINK START! (https://github.com/evnchn/linkstart.py)
+for line in import_string.splitlines():
+    if "import" in line:
+        print(line)
+        try:
+            exec(line)
+        except:
+            if "#" in line:
+                package_name = line.split("#")[-1]
+            else:
+                splits = line.split("import")
+                if "from" in line:
+                    package_name = splits[0].replace("from","")
+                else:
+                    package_name = splits[1]
+            package_name = package_name.strip()
+            print("Installing {}...".format(package_name))    
+            import subprocess
+            import sys
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+            try:
+                exec(line)
+            except:
+                print("Failed to install {}".format(package_name))
+### DONE
+import minibus
+import stop
+import vis
+import random
+import os
+import subprocess
+import openpyxl as xl # openpyxl
 
 ### const ###
 MAX_TIME = 10000
@@ -102,5 +136,5 @@ while True:
   break  
 
 wb.close()
-print("Simulation finished")
+print("\nSimulation finished")
 subprocess.run("visualization_time.xlsx", shell=True)
